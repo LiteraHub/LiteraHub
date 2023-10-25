c9e97a28bd0603d585dce501ce6516052e0ba830
mohammadfauzan21 <mohammad.fauzan21@ui.ac.id>
2023-10-25 15:36:42 +0700

    Mengupdate settings.py pada installed apps di project

--- LiteraHub/settings.py
+++ LiteraHub/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'daftarbuku',
+    'forum',
+    'lembarasa',
+    'peminjamanbuku',
+    'reservasi',
 ]
 
 MIDDLEWARE = [
