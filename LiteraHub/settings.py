8b76f9dff526a92a727da446f280ebd78455485f
putiraissa <puti.raissa@ui.ac.id>
2023-10-25 15:51:46 +0700

    menambahkan url di url.py

--- LiteraHub/settings.py
+++ LiteraHub/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'lembarasa',
     'peminjamanbuku',
     'reservasi',
+    'buku',
 ]
 
 MIDDLEWARE = [
--- LiteraHub/urls.py
+++ LiteraHub/urls.py
@@ -20,5 +20,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
+    path('lembarasa/', include('lembarasa.urls')),
+    path('forum/', include('forum.urls')),
+    path('daftarbuku/', include('daftarbuku.urls')),
+    path('peminjamanbuku/', include('peminjamanbuku.urls')),
+    path('reservasi/', include('reservasi.urls')),
+    path('api/buku/', include('buku.urls')),
 ]
