00310a48aa5d688848beac56c3d300842a8daf55
jteo0 <jjtheodora@gmail.com>
2023-10-29 18:02:51 +0700

    working forum

--- LiteraHub/settings.py
+++ LiteraHub/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'main',
     'daftarbuku',
     'forum',
@@ -124,8 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = (os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
 )
+
 
 
 # Default primary key field type
