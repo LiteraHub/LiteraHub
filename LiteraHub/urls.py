d74b0d242ae5397376a261f1cd0110511ade761d
mohammadfauzan21 <mohammad.fauzan21@ui.ac.id>
2023-10-29 20:03:08 +0700

    modul daftarbuku

--- LiteraHub/urls.py
+++ LiteraHub/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', include('main.urls')),
     # path('lembarasa/', include('lembarasa.urls')),
     # path('forum/', include('forum.urls')),
-    # path('daftarbuku/', include('daftarbuku.urls')),
+    path('daftarbuku/', include('daftarbuku.urls')),
     # path('peminjamanbuku/', include('peminjamanbuku.urls')),
     # path('reservasi/', include('reservasi.urls')),
     path('api/buku/', include('buku.urls')),
