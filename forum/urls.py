f08302e4088ef66ce60242c59ae1d288fb3d1b1b
jteo0 <jjtheodora@gmail.com>
2023-10-29 20:36:29 +0700

    forum merging

--- forum/urls.py
+++ forum/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from forum.views import show_forum, add_post
+from . import views
+from forum.views import show_forum, add_post, add_thread, trending, writer, explore, to_thread, get_threads, get_books_json, get_post, get_time
 
 app_name = 'forum'
 
 urlpatterns = [
     path('', show_forum, name='show_forum'),
-    path('add_post', add_post, name='add_post'),
+    path('add_post', views.add_post, name='add_post'),
+    path('add_thread', views.add_thread, name='add_thread'),
+    path('forum_trending/', trending, name='forum_trending'),
+    path('forum_explore/', explore, name='forum_explore'),
+    path('forum_writers/', writer, name='forum_writers'),
+    path('threads/', to_thread, name='threads'),
+    path('get_threads/', views.get_threads, name='get_threads'),
+    path('get_books_json/', get_books_json, name='get_books_json'),
+    path('get_post/', views.get_post, name='get_post'),
+    path('get_time/', get_time, name='get_time'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound, HttpResponseRedirect, HttpResponse
 import datetime
-from forum.models import Buku, Forum, Thread, Post
+from django.db.models import Count
+from django.contrib.auth.decorators import login_required
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from forum.models import Thread, Post
+from buku.models import Buku
 
 def show_forum(request):
-    forums = Forum.objects.all()
-    thread = Thread.objects.all()
+    threads = Thread.objects.filter(forum="recommend")
+    posts = Post.objects.all()
     
     context = {
+        'threads': threads,
+        'posts': posts,
     }
 
     return render(request, "forum.html", context)
 
+@login_required(login_url='/login')
+@csrf_exempt
 def add_post(request):
     if request.method == 'POST':
+        thread = request.thread
         body = request.POST.get("body")
-        thread = request.POST.get("thread")
         date = str(datetime.datetime.now())
         user = request.user
 
@@ -24,3 +33,84 @@ def add_post(request):
 
         return HttpResponse(b"CREATED", status=201)
     return HttpResponseNotFound()
+
+@login_required(login_url='/login')
+@csrf_exempt
+def add_thread(request):
+    if request.method == 'POST':
+        author = request.user
+        forum = request.POST.get("forum")
+        name = request.POST.get("name")
+        date = str(datetime.datetime.now())
+        buku_title = request.POST.get("buku")
+        
+        try:
+            buku = Buku.objects.get(title=buku_title)
+        except Buku.DoesNotExist:
+            buku = None
+
+        new_thread = Thread(author=author, name=name, forum=forum, date=date, buku=buku)
+        new_thread.save()
+
+        return HttpResponse(b"CREATED", status=201)
+    return HttpResponseNotFound()
+
+
+@login_required(login_url='/login')
+def writer(request):
+    threads = Thread.objects.filter(forum="penulis")
+    posts = Post.objects.all()
+    
+    context = {
+        'threads': threads,
+        'posts': posts,
+    }
+
+    return render(request, "forum_writers.html", context)
+
+@login_required(login_url='/login')
+def trending(request):
+    threads = Thread.objects.annotate(post_count=Count('post')).order_by('-post_count')
+    posts = Post.objects.all()
+    
+    context = {
+        'threads': threads,
+        'posts': posts,
+    }
+    return render(request, "forum_trending.html", context)
+
+@login_required(login_url='/login')
+def explore(request):
+    threads = Thread.objects.all()
+    posts = Post.objects.all()
+    
+    context = {
+        'threads': threads,
+        'posts': posts,
+    }
+    return render(request, "forum_explore.html", context)
+
+def to_thread(request):
+    threads = Thread.objects.all()
+    posts = Post.objects.all()
+    
+    context = {
+        'threads': threads,
+        'posts': posts,
+    }
+    return render(request, "threads.html", context)
+
+def get_threads(request):
+    threads = Thread.objects.all()
+    return HttpResponse(serializers.serialize('json', threads))
+
+def get_post(request):
+    posts = Post.objects.all()
+    return HttpResponse(serializers.serialize('json', posts))
+
+def get_books_json(request):
+    books = Buku.objects.all()
+    return HttpResponse(serializers.serialize('json', books), content_type="application/json")
+
+def get_time(request):
+    return str(datetime.datetime.now())
