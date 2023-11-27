240739f6236a1b8ebcc720f16fbeaf8bc79bf66e
mohammadfauzan21 <mohammad.fauzan21@ui.ac.id>
2023-11-27 16:09:25 +0700

    Nambahin django-cors-headers

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
+from django.contrib.auth.models import User
 
 @csrf_exempt
 def login(request):
@@ -29,4 +31,33 @@ def login(request):
         return JsonResponse({
             "status": False,
             "message": "Login gagal, periksa kembali email atau kata sandi."
+        }, status=401)
+    
+@csrf_exempt  
+def register(request):
+    data = json.loads(request.body)
+    username = data['username']
+    password = data['password']
+    repassword = data['repassword']
+    print(username)
+
+    if(password==repassword):
+        try:
+            user= User.objects.create_user(username=username,password=password)
+            if(user!=None):
+                user.save()
+                return JsonResponse({
+                "username": user.username,
+                "status": True,
+                "message": "Your account has been successfully created!"
+            }, status=200)
+        except:
+                return JsonResponse({
+                "status": False,
+                "message": "Registration Failed. The name has already been used."
+                }, status=401)
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Registration Failed. Check your password."
         }, status=401)
